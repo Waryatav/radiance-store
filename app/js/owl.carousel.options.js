a5e89713d9d96f8e4e906feae00101b8a3dc5592
Anton <waryatav@gmail.com>
2016-09-02 16:01:12 +0300

    top carousel

--- app/js/owl.carousel.options.js
+++ app/js/owl.carousel.options.js
@@ -1,3 +1,10 @@
 jQuery(document).ready(function($) {
-	
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        nav:true,
+         items:1,
+        navText : ["<img src='/img/icons/slider-top-prev.png'>","<img src='/img/icons/slider-top-next.png'>"]
+    });
+
 });
