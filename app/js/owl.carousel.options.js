cc68888e7355dc670933e693e98264ff1737e0fd
Anton <waryatav@gmail.com>
2016-09-05 17:05:12 +0300

    slider bot

--- app/js/owl.carousel.options.js
+++ app/js/owl.carousel.options.js
@@ -1,10 +1,17 @@
 jQuery(document).ready(function($) {
 
-    $('.owl-carousel').owlCarousel({
+    $('.slider-top').owlCarousel({
         loop:true,
         nav:true,
          items:1,
         navText : ["<img src='/img/icons/slider-top-prev.png'>","<img src='/img/icons/slider-top-next.png'>"]
+    });
+
+    $('.slider-bot').owlCarousel({
+        loop:true,
+        nav:true,
+        items:1,
+        navText : ["<img src='/img/icons/slider-bot-prev.png'>","<img src='/img/icons/slider-bot-next.png'>"]
     });
 
 });
