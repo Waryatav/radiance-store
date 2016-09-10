7abfaa731961fcaff14f100bec93f684f3bb0111
Anton <waryatav@gmail.com>
2016-09-10 12:08:17 +0300

    js menu move

--- app/js/script.js
+++ app/js/script.js
@@ -4,11 +4,25 @@ jQuery(document).ready(function($) {
       $(catalogue_li).each(function (i, catalogue_li) {
                if(i<6){
                    $(catalogue_li).addClass("visible-item");
+               }else {
+                   $(catalogue_li).css({"display":"none"});
                }
 
      });
 
-     $('.catalogue_arrow_down').click(function () {
+    var custom_care_ul = $(".custom_care__menu");
+    var custom_care_li= custom_care_ul.children("li");
+    $(custom_care_li).each(function (i, custom_care_li) {
+        if(i<4){
+            $(custom_care_li).addClass("visible-item");
+        }else {
+            $(custom_care_li).css({"display":"none"});
+        }
+
+    });
+
+
+       $(document).on("click", ".catalogue_arrow_down", function () {
          event.preventDefault();
          var allElements = cataljgue_ul.find("li");
          //console.log(allElements);
@@ -18,15 +32,15 @@ jQuery(document).ready(function($) {
          //console.log(visibleElements);
 
          var index = visibleElements.length+allHiddenElements.length;
-         console.log(index);
-         console.log(allElements.length);
+
          if(index < allElements.length){
-             $(visibleElements[0]).removeClass("visible-item").addClass('hidden');
-             $(allElements[visibleElements.length+allHiddenElements.length]).addClass("visible-item");
+
+             $(visibleElements[0]).removeClass("visible-item").slideUp().addClass('hidden');
+             $(allElements[visibleElements.length+allHiddenElements.length]).slideDown().addClass("visible-item");
          }else{
              return false;
          }
-     })
+     });
 
     $(document).on("click",".catalogue_arrow_up",function () {
         event.preventDefault();
@@ -34,11 +48,45 @@ jQuery(document).ready(function($) {
         
         var visibleElements = $(".catalogue_menu").find(".visible-item");
         if(allHiddenElements.length>0){
-            $(allHiddenElements[allHiddenElements.length-1]).removeClass("hidden").addClass("visible-item");
-            $(visibleElements[visibleElements.length-1]).removeClass("visible-item");
+            $(allHiddenElements[allHiddenElements.length-1]).slideDown().removeClass("hidden").addClass("visible-item");
+            $(visibleElements[visibleElements.length-1]).slideUp().removeClass("visible-item");
 
         }
         
+
+    });
+
+    //custom care
+    $(document).on("click", ".custom_care__arrow_down", function () {
+        event.preventDefault();
+        var allElements = custom_care_ul.find("li");
+
+        var allHiddenElements = custom_care_ul.find(".hidden");
+
+        var visibleElements = $(".custom_care__menu").find(".visible-item");
+
+        var index = visibleElements.length+allHiddenElements.length;
+
+        if(index < allElements.length){
+
+            $(visibleElements[0]).removeClass("visible-item").slideUp().addClass('hidden');
+            $(allElements[visibleElements.length+allHiddenElements.length]).slideDown().addClass("visible-item");
+        }else{
+            return false;
+        }
+    });
+
+    $(document).on("click",".custom_care__arrow_up",function () {
+        event.preventDefault();
+        var allHiddenElements = custom_care_ul.find(".hidden");
+
+        var visibleElements = $(".custom_care__menu").find(".visible-item");
+        if(allHiddenElements.length>0){
+            $(allHiddenElements[allHiddenElements.length-1]).slideDown().removeClass("hidden").addClass("visible-item");
+            $(visibleElements[visibleElements.length-1]).slideUp().removeClass("visible-item");
+
+        }
+
 
     });
 });
