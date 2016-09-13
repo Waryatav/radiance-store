a416e26740376fff011fae55f1ef428138eda3bd
Anton <waryatav@gmail.com>
2016-09-13 12:42:24 +0300

    slider

--- app/js/slick.carousel.options.js
+++ app/js/slick.carousel.options.js
@@ -1,7 +1,6 @@
 jQuery(document).ready(function($) {
     $(".slick-slider").slick({
         asNavFor: '.slider-top',
-        infinite: true,
         slidesToShow: 3,
         slidesToScroll: 1,
         dots: false,
@@ -10,7 +9,8 @@ jQuery(document).ready(function($) {
 });
     $(".slider-top").slick({
         asNavFor: '.slick-slider',
-        nextArrow:"<img class='slider-top-prev' src='img/icons/slider-top-prev.png'>",
-        prevArrow:"<img class='slider-top-next' src='img/icons/slider-top-next.png'>"
+        nextArrow:"<img class='slider-top-prev' src='img/icons/slider-top-next.png'>",
+        prevArrow:"<img class='slider-top-next' src='img/icons/slider-top-prev.png'>"
+
     });
 });
