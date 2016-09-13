8a100f8e74b2b44e2be03e37852b784c16546adb
Anton <waryatav@gmail.com>
2016-09-13 15:39:07 +0300

    responsive

--- app/js/slick.carousel.options.js
+++ app/js/slick.carousel.options.js
@@ -5,12 +5,14 @@ jQuery(document).ready(function($) {
         slidesToScroll: 1,
         dots: false,
         vertical: true,
-        arrows: false
+        arrows: false,
+        verticalSwiping: true
 });
     $(".slider-top").slick({
         asNavFor: '.slick-slider',
         nextArrow:"<img class='slider-top-prev' src='img/icons/slider-top-next.png'>",
-        prevArrow:"<img class='slider-top-next' src='img/icons/slider-top-prev.png'>"
+        prevArrow:"<img class='slider-top-next' src='img/icons/slider-top-prev.png'>",
+        verticalSwiping: true
 
     });
 });
